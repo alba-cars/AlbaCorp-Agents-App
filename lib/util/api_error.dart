f945ecf96f8bc956254c6766d5d31464cc94f13f
Ismail <ismail@albacars.ae>
2024-12-05 13:21:35 +0400

    newtoken

--- lib/util/api_error.dart
+++ lib/util/api_error.dart
@@ -32,7 +32,7 @@ Result<T> onError<T>(Object e, StackTrace stack, Logger log) {
 class SlackService {
   final Dio _dio = Dio();
   static const String _slackWebHookUrl =
-      'https://hooks.slack.com/services/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';
+      'https://hooks.slack.com/services/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';
 
   Future<void> sendSlackMessage({
     required String message,
