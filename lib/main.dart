8cd2c82b16335302f56897b09b28e8f3031d2218
ismail <ismail@albacars.ae>
2025-06-04 10:26:12 +0400

    gitignore

--- lib/main.dart
+++ lib/main.dart
@@ -65,7 +65,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await dotenv.load(fileName: ".env");
-  await configureDependencies(env: 'Prod');
+  await configureDependencies(env: 'Stage');
   // TwilioVoiceFlutter.init();
   runApp(const App());
 }
