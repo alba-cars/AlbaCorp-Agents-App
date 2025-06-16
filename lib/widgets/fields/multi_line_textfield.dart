5787fb96ef2d4e040a12f8e516c5693df872ac62
Ismail Farisi <ismail@albacars.ae>
2025-06-16 11:56:55 +0400

    ff

--- lib/widgets/fields/multi_line_textfield.dart
+++ lib/widgets/fields/multi_line_textfield.dart
@@ -62,11 +62,11 @@ class _MultiLineFieldState extends State<MultiLineField> {
     controller = widget.controller ?? TextEditingController();
     _focusNode.addListener(_onFocusChanged);
     SchedulerBinding.instance.addPostFrameCallback((timeStamp) {
-      if (controller.text.isNotEmpty) {
-        _fieldKey.currentState?.setValue(controller.text);
-      }
       if (_fieldKey.currentState?.value != null) {
         controller.text = _fieldKey.currentState?.value ?? '';
+      }
+      if (controller.text.isNotEmpty) {
+        _fieldKey.currentState?.didChange(controller.text);
       }
     });
 
